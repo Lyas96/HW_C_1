d9e1ba0c59232e85eb4642cfcfc9b0ae44f91cac
Lyas <Egorlyasotsky@yandex.ru>
2023-03-27 15:31:26 +0400

    Решена вторая задача

---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа принимает на вход три числа и выдает максимальное из них
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine($"Наибольшее число равно: {max}");
