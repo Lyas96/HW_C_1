74afae172ccd2c4e962dbdbfd82684119b13737d
Lyas <Egorlyasotsky@yandex.ru>
2023-03-27 16:08:49 +0400

    Решена четвертая задача

---
+++ task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа на вход принимает число N и выдает все четные от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+if(i % 2 == 0)
+{
+  Console.Write($"{i}, ");
+}
